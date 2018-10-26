41e054ce6d37a7229b8d3481ae9b2e1f814a2c37
AkiyukiKamiura <jgr9ey2ug8@gmail.com>
2018-10-27 01:47:35 +0900

    precompiling error

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module TradeSimulator
     config.load_defaults 5.2
 
     config.autoload_paths += %W(#{config.root}/lib)
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
