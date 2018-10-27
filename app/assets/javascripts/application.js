ec0fe3264919f8b6e0561256980f57a47f4560fe
AkiyukiKamiura <jgr9ey2ug8@gmail.com>
2018-10-27 17:53:45 +0900

    change basic design

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,39 +8,18 @@
 
 ELEMENT.locale(ELEMENT.lang.ja)
 
-window.tradeChart = Vue.extend({
-  template: "\
-    <el-card class='trade-chart' shadow='hover'>\
-      <div slot='header' class='clearfix'>\
-        <span>GENERAL STATISTIC</span>\
-      </div>\
-      <el-row>\
-        <el-col :span='6' style='height: 100px;'>\
-          <div class='currencies'>USD/JPY</div>\
-        </el-col>\
-        <el-col :span='6' style='height: 70px;'>\
-          <div class='values'>{{ this.timeSeries[0]['close'] }}</div>\
-        </el-col>\
-        <el-col :span='6' style='height: 70px;'>\
-          <div class='values'>あれ</div>\
-        </el-col>\
-        <el-col :span='6' style='height: 70px;'>\
-          <div class='values'>あれ</div>\
-        </el-col>\
-      </el-row>\
-      <div class='block'>\
-        <el-cascader expand-trigger='hover' :options='analyticsOptions' @change='handleSelectAnalytics'></el-cascader>\
-      </div>\
-      <div id='myChart' style='height: 800px;'></div>\
-    </el-card>",
+window.tradeApp = new Vue({
+  el: "#trade-app",
+  components: {
+  },
   data: function() {
     return {
-      text: "あいうえお",
       timeSeries: {},
       showMovingAverage: false,
       movingAverageType: 'simple', // type: simple, exp
       movingAverageWindowLen: 21,
-      analyticsOptions: [{
+      lowAnalyticsOptions: [
+        {
         value: 'moving_average',
         label: '移動平均線',
         children: [{
@@ -60,80 +39,75 @@ window.tradeChart = Vue.extend({
           value: 'sigma-2',
           label: '2σ'
         }]
-      }]
+      }],
+      highAnalyticsOptions: [
+        {
+          value: 'trending',
+          label: 'トレンド判定',
+          children: [{
+            value: 'dow',
+            label: 'ダウ理論'
+          }]
+        }
+      ],
+
+      chartElementId: 'myChart'
     }
   },
   mounted: function () {
+    console.log('mounted')
     axios.get('/api/forex_timeseries/get_timeseries').then(response => { this.timeSeries = response.data })
   },
   updated: function () {
+    console.log('updated')
     this.drawChart()
   },
   methods: {
-    handleSelectAnalytics: function (value) {
+    handleSelectLowAnalytics: function (value) {
       console.log(value)
     },
-    drawChart: function () {
-      var trace1 = {
-        increasing: {line: {color: '#17BECF'}},
-        decreasing: {line: {color: '#7F7F7F'}},
-        line: {color: 'rgba(31,119,180,1)'},
-        type: 'candlestick',
-        xaxis: 'x',
-        yaxis: 'y',
-        x: [],
-        close: [],
-        high: [],
-        low: [],
-        open: []
-      }
-      for (let i in this.timeSeries){
-        let v = this.timeSeries[i]
-        trace1['x'].push(v['time'])
-        trace1['open'].push(v['open'])
-        trace1['close'].push(v['close'])
-        trace1['high'].push(v['high'])
-        trace1['low'].push(v['low'])
-      }
-
-      var data = [trace1];
-
-      var layout = {
-        dragmode: 'zoom',
-        margin: {
-          r: 10,
-          t: 25,
-          b: 40,
-          l: 60
+    handleSelectHighAnalytics: function (value) {
+      console.log(value)
+    },
+    drawChart: function() {
+      console.log('drawChart')
+      var ctx = document.getElementById(this.chartElementId).getContext('2d');
+      var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
         },
-        showlegend: false,
-        xaxis: {
-          autorange: true,
-          domain: [0, 1],
-          range: [trace1['x'].first, trace1['x'].last],
-          rangeslider: {range: [trace1['x'].first, trace1['x'].last]},
-          title: 'Date',
-          type: 'date'
-        },
-        yaxis: {
-          autorange: true,
-          domain: [0, 1],
-          range: [114.609999778, 137.410004222],
-          type: 'linear'
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
         }
-      };
-
-      Plotly.plot('myChart', data, layout);
-
+    });
     }
-  }
-})
-
-window.tradeApp = new Vue({
-  el: "#trade-app",
-  components: {
-    "trade-chart": window.tradeChart
-  },
-  mounted: function() {
   }
 })
